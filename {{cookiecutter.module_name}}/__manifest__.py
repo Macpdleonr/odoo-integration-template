f3098895a26d8d2a70b0f16b2c42f782669f481e
Mac <macpdleonr@gmail.com>
2025-09-24 15:38:55 -0500

    Update __manifest__.py

--- {{cookiecutter.module_name}}/__manifest__.py
+++ {{cookiecutter.module_name}}/__manifest__.py
@@ -2,12 +2,23 @@
     "name": "{{ cookiecutter.module_title }}",
     "version": "{{ cookiecutter.version }}",
     "summary": "{{ cookiecutter.summary }}",
+    "description": """
+        {{cookiecutter.module_name}}
+        ============================
+        Integration module in Odoo to connect with external services.
+    """,
     "author": "{{ cookiecutter.author }}",
+    "website": "https://github.com/{{cookiecutter.github_user}}",
+    "category": "Integration",
     "license": "{{ cookiecutter.license }}",
     "depends": ["base", "web"],
     "data": [
+        "security/security.xml",
         "security/ir.model.access.csv",
-        "views/integration_settings_views.xml"
+        "views/integration_settings_views.xml",
+        "data/ir_cron_data.xml",
     ],
     "installable": True,
+    "application": False,
+    "license": "MIT",
 }
