db607aad4afbc4bd6c9614b0479950d59ff7ac7a
Mac <macpdleonr@gmail.com>
2025-09-29 15:49:51 -0500

    Update res_config_settings.py

--- {{cookiecutter.module_name}}/models/res_config_settings.py
+++ {{cookiecutter.module_name}}/models/res_config_settings.py
@@ -4,41 +4,43 @@ class ResConfigSettings(models.TransientModel):
     
     _inherit = 'res.config.settings'
      
-    UserName = fields.Char(
-        string="UserName",
-        help="Username",
-        config_parameter="username"
+    {{cookiecutter.module_name}}_userName = fields.Char(
+        string="{{cookiecutter.module_title}} UserName",
+        help="{{cookiecutter.module_title}} Username",
+        config_parameter="{{cookiecutter.module_name}}.username"
     )
 
-    password = fields.Char(
-        string="Password",
-        help="Password",
-        config_parameter="password"
+    {{cookiecutter.module_name}}_password = fields.Char(
+        string="{{cookiecutter.module_title}} Password",
+        help="{{cookiecutter.module_title}} Password",
+        config_parameter="{{cookiecutter.module_name}}.password"
+    )
+
+    {{cookiecutter.module_name}}_base_url = fields.Char(
+        string="{{cookiecutter.module_title}} Base URL",
+        help="Root URL of the {{cookiecutter.module_title}} System.\n"
+         "This will be used as the base to build all API endpoints.",
+        config_parameter="{{cookiecutter.module_name}}.base_url"
     )
      
     @api.model
     def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
+        res = super().get_values()
 
         ICPSudo = self.env['ir.config_parameter'].sudo()
 
-        username = ICPSudo.get_param('username')
-        password = ICPSudo.get_param('password')
+        password = ICPSudo.get_param('{{cookiecutter.module_name}}.password')
 
         res.update(
-            username = username or '',
-            password = '*' * (len(password) if password else 0) if password else ''
+            {{cookiecutter.module_name}}_password = '*' * (len(password) if password else 0) if password else '',
         )
 
         return res
      
     def set_values(self):
-        super(ResConfigSettings, self).set_values()
+        super().set_values()
         ICPSudo = self.env['ir.config_parameter'].sudo()
 
-        if self.password and \
-           self.password != '*' * len(self.password):
-            ICPSudo.set_param('password', self.password)
-            
-            
-        ICPSudo.set_param('username', self.username)
+        if self.{{cookiecutter.module_name}}_password and \
+           self.{{cookiecutter.module_name}}_password != '*' * len(self.{{cookiecutter.module_name}}_password):
+            ICPSudo.set_param('{{cookiecutter.module_name}}.password', self.{{cookiecutter.module_name}}_password)
